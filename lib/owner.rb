c3c52998849ad2a0065b4fffa145dbd0c9c01816
Eric Fiorani <efiorani305@gmail.com>
2018-05-14 21:35:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
   # code goes here
+  attr_accessor :pets
+
+  pets => {
+    fishes: [],
+    cats: [],
+    dogs: []
+  }
+
+  def initialize(pets)
+    # @pets = pets => {fishes: [], cats: [], dogs: []}
+  end
+
 end
